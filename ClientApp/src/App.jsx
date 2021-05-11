793c62a86df98673b2f99bc616c41f5b77f0a681
Amanda Leaders <alsleaders@gmail.com>
2021-05-11 15:06:25 -0400

    css improved, input field next

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import './custom.scss'
 import Container from './components/Container'
+import Greetings from './components/Greetings'
 
 export function App() {
-  return <Container />
+  return (
+    <>
+      <Greetings />
+      <Container />
+    </>
+  )
 }
---
+++ ClientApp/src/components/Greetings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Greetings extends Component {
+  render() {
+    return (
+      <div>
+        <h1>These are my books!</h1>
+      </div>
+    )
+  }
+}
+
+export default Greetings
